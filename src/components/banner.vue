212f2e3ac07da4736f0c74955dcf349ab2d5d6f3
kenryuS <kenryushibata@gmail.com>
2023-03-12 15:07:44 -0400

    started dev

---
+++ src/components/banner.vue
@@ -0,0 +1,27 @@
+<script>
+
+</script>
+
+<template>
+    <div class="banner">
+        <h1>Japan Visitation Record</h1>
+        <p>The Diary I put on the first visit to Japan for 5 years.</p>
+    </div>
+</template>
+
+<style>
+    .banner {
+        height: 100vh;
+        background: top url(/banner.jpg) no-repeat;
+        background-size: cover;
+    }
+
+    .banner h1 {
+        padding-top: 50lvh;
+    }
+
+    .banner p {
+        font-size: larger;
+        font-weight: bold;
+    }
+</style>
